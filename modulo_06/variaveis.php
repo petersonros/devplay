8df7b94cb2ec4b79401820d2aa7dcbcb17fca912
Peterson <petersonros@hotmail.com>
2022-11-24 15:42:02 -0300

    modulo 06

---
+++ modulo_06/variaveis.php
@@ -0,0 +1,15 @@
+<?php
+
+// COMENTÁRIO DE LINHA ÚNICA
+# COMENTÁRIO DE LINHA ÚNICA  (ALTERNATIVA)
+//Atribuindo o nome "Jhonatan" que é do tipo STRING à variável $nome.
+$nome = "Jhonatan";
+//Atribuindo o valor 1.83 que é do tipo DOUBLE/FLOAT à variável $altura.
+$altura = 1.83;
+//Atribuindo o valor true que é do tipo BOOLEAN à variável $brasileiro.
+$brasileiro = true;
+
+echo "Olá, mundo!\n";
+echo "Olá, meu nome é $nome.\nTenho $idade anos! Meço $altura.\nSou ";
+echo $brasileiro ? "brasileiro" : "estrangeiro";
+echo "\n";
