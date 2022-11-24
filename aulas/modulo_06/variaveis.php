d0acd629933202012f3ba87009b61e0f3d7acc3a
Peterson <petersonros@hotmail.com>
2022-11-24 16:09:01 -0300

    atribuindo valores nas variaveis

--- aulas/modulo_06/variaveis.php
+++ aulas/modulo_06/variaveis.php
@@ -2,12 +2,14 @@
 
 // COMENTÁRIO DE LINHA ÚNICA
 # COMENTÁRIO DE LINHA ÚNICA  (ALTERNATIVA)
-//Atribuindo o nome "Jhonatan" que é do tipo STRING à variável $nome.
-$nome = "Jhonatan";
-//Atribuindo o valor 1.83 que é do tipo DOUBLE/FLOAT à variável $altura.
-$altura = 1.83;
+//Atribuindo o nome "Peterson" que é do tipo STRING à variável $nome.
+$nome = "Peterson";
+//Atribuindo o valor 1.65 que é do tipo DOUBLE/FLOAT à variável $altura.
+$altura = 1.65;
 //Atribuindo o valor true que é do tipo BOOLEAN à variável $brasileiro.
 $brasileiro = true;
+
+$idade = 43;
 
 echo "Olá, mundo!\n";
 echo "Olá, meu nome é $nome.\nTenho $idade anos! Meço $altura.\nSou ";
