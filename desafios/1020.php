ae481f2a4f801f7573e7697d65116d956d0d84a5
Peterson <petersonros@hotmail.com>
2022-11-25 14:25:16 -0300

    desafio 1020 finalizado

--- desafios/1020.php
+++ desafios/1020.php
@@ -5,14 +5,21 @@
 echo "Digite um número para ser convertido em anos, meses e dias: ";
 $number = trim(fgets(STDIN));
 
+// número dividido por 365 igual a ano(os).
 $year = $number / 365;
 
+// pegar resto da divisão do número por 365.
 $year_modulo = $number % 365;
 
+// para ter o número de meses, dividir por 30 a sobra da divisão do número por 365.
 $month = $year_modulo / 30;
 
-$days = $number % 30;
+// pegar a sobra da abstração do número de meses.
+$month_modulo = $year_modulo % 30;
 
-echo "\nSeu número corresponde a ".(int)$year." anos, ".(int)$month." meses e $days dias";
+// pegar sobra da divisão pra encontrar o número de meses.
+$days = $month_modulo % 30;
+
+echo "\nSeu número corresponde a ".(int)$year." ano(os), ".(int)$month." mês(es) e $days dia(s)";
 
 ?>
