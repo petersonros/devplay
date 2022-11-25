8bfbbbed64b424780d5135b9e67e518fb1f650b5
Peterson <petersonros@hotmail.com>
2022-11-25 11:35:11 -0300

    challenge 1020.php

--- desafios/1020.php
+++ desafios/1020.php
@@ -1,7 +1,18 @@
 <?php
 
-// converter idade em dias
+// converter um número inteiro em anos, meses e dias.
 
+echo "Digite um número para ser convertido em anos, meses e dias: ";
+$number = trim(fgets(STDIN));
 
+$year = $number / 365;
+
+$year_modulo = $number % 365;
+
+$month = $year_modulo / 30;
+
+$days = $number % 30;
+
+echo "\nSeu número corresponde a ".(int)$year." anos, ".(int)$month." meses e $days dias";
 
 ?>
